98c97382e6e62a496de6c0a0217a6fb7db0593cf
Robin Nook <robin.nook@yolo.com>
2023-07-13 13:17:56 +0300

    STUDY: Atoms and their how to notate them properly

--- Elixir in Action/shell_testing/shell/lib/shell.ex
+++ Elixir in Action/shell_testing/shell/lib/shell.ex
@@ -1,35 +1,15 @@
 defmodule Shell do
   import IO
 
-  def numbers do
+  def atoms do
     puts "---------------------------------------------------"
-    puts "Integer"
-    puts 17
+    puts "Atoms"
+    puts :atom
+    puts :another_atom
+    puts :"an atom with spaces"
     puts "---------------------------------------------------"
-    puts "Integer written in HEX"
-    puts 0xEF
-    puts "---------------------------------------------------"
-    puts "Float"
-    puts 9.8
-    puts "---------------------------------------------------"
-    puts "Float with exponential notation"
-    puts 99.0e-5
-    puts "---------------------------------------------------"
-  end
-
-  def arithmetic do
-    puts "---------------------------------------------------"
-    puts "Returns an integer (5 + 3 * 10)"
-    puts 5 + 3 * 10
-    puts "---------------------------------------------------"
-    puts "Always returns a float (10 / 5)"
-    puts 10 / 5
-    puts "---------------------------------------------------"
-    puts "Integer division (4 // 2)"
-    puts div(4, 2)
-    puts "---------------------------------------------------"
-    puts "Integer remainder (35 % 3)"
-    puts rem(35, 3)
+    puts "Atoms are used for naming constants"
+    puts variable = :variable
     puts "---------------------------------------------------"
   end
 end
