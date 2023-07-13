79eef4047b05d916d8d6825ce681a369c4f89999
Robin Nook <robin.nook@yolo.com>
2023-07-13 10:26:14 +0300

    STUDY: Alias for imported modules 2

--- Elixir in Action/shell_testing/shell/lib/shell.ex
+++ Elixir in Action/shell_testing/shell/lib/shell.ex
@@ -1,8 +1,17 @@
 defmodule Shell do
   alias IO, as: NewIO
+  alias Shell.Second # Second is used as alias
 
   # Using the new alias for the IO module.
   def fun do
     NewIO.puts "Hi!"
+    Second.fun()
+  end
+end
+
+
+defmodule Shell.Second do
+  def fun do
+    2 + 2
   end
 end
