b830766e388aa1970c2d39bbb3a2bfd1cb855460
Robin Nook <robin.nook@yolo.com>
2023-07-13 13:41:52 +0300

    STUDY: Truthy and Falsy values, short circuits

--- Elixir in Action/shell_testing/shell/lib/shell.ex
+++ Elixir in Action/shell_testing/shell/lib/shell.ex
@@ -1,22 +1,20 @@
 defmodule Shell do
   import IO
 
-  def booleans do
+  def truthy_and_falsy_values do
     puts "---------------------------------------------------"
-    puts ":true == true"
-    puts :true == true
+    puts "nil and false are both considered falsy values, all other values are considered truthy"
+    puts "|| operator returns first operator that isn't falsy (nil || false || 10 || true)"
+    puts (nil || false || 10 || true)
     puts "---------------------------------------------------"
-    puts ":false == false"
-    puts :false == false
+    puts "When all values are falsy, the || operator returns the last given value (nil || false)"
+    puts (nil || false)
     puts "---------------------------------------------------"
-    puts "true and false"
-    puts true and false
+    puts "&& operator returns the second given value only if the first one is truthy (:an_atom && 5)"
+    puts (:an_atom && 5)
     puts "---------------------------------------------------"
-    puts "true or false"
-    puts true or false
-    puts "---------------------------------------------------"
-    puts "not false:"
-    puts not false
+    puts "When the first given value is falsy, the first given value is returned instead (false && true)"
+    puts (false && true)
     puts "---------------------------------------------------"
   end
 end
