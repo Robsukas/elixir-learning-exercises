626028ab591e56af2e62642f258bdca93e3abadc
Robin Nook <robin.nook@yolo.com>
2023-07-13 14:27:53 +0300

    STUDY: Lists and opertions on them

--- Elixir in Action/shell_testing/shell/lib/shell.ex
+++ Elixir in Action/shell_testing/shell/lib/shell.ex
@@ -1,22 +1,29 @@
 defmodule Shell do
   import IO
-  # Changed the system of code writing a bit, as pasting in line breaks got a bit too tedious.
+  # Changed the system of code writing a bit,
+  # as pasting in big line breaks and comments got a bit too tedious.
   # Also the code wasn't that well readable with those anyway.
   # With this I also make the workflow more time-efficient.
 
-  def tuples do
-    person = {"Robin", 20}
-    age = elem(person, 1)
-    name = elem(person, 0)
-
-    puts name
-    puts age
-
-    person = put_elem(person, 0, "Robin Nook")
-    name = elem(person, 0)
-
-    puts " "
-    puts name
-    puts age
+  def lists do
+    # Most operations on lists have an O(n) complexity.
+    numbers = [1, 2, 3, 4]
+    IO.puts length(numbers)
+    IO.inspect numbers
+    IO.puts " "
+    IO.puts Enum.at(numbers, 0)
+    IO.puts " "
+    IO.puts 5 in numbers
+    IO.puts 3 in numbers
+    IO.puts " "
+    numbers = List.replace_at(numbers, 0, 10)
+    IO.inspect numbers
+    IO.puts " "
+    numbers = List.insert_at(numbers, -1, 0)
+    IO.inspect numbers
+    IO.puts " "
+    numbers2 = [11, 14]
+    IO.inspect numbers ++ numbers2
+    IO.puts " "
   end
 end
