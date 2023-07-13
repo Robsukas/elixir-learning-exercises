02a9ba39b69011726a29dc3ae7aa7b0ccbc1057b
Robin Nook <robin.nook@yolo.com>
2023-07-13 14:03:24 +0300

    STUDY: Tuples and changing elements in them

--- Elixir in Action/shell_testing/shell/lib/shell.ex
+++ Elixir in Action/shell_testing/shell/lib/shell.ex
@@ -1,20 +1,22 @@
 defmodule Shell do
   import IO
+  # Changed the system of code writing a bit, as pasting in line breaks got a bit too tedious.
+  # Also the code wasn't that well readable with those anyway.
+  # With this I also make the workflow more time-efficient.
 
-  def truthy_and_falsy_values do
-    puts "---------------------------------------------------"
-    puts "nil and false are both considered falsy values, all other values are considered truthy"
-    puts "|| operator returns first operator that isn't falsy (nil || false || 10 || true)"
-    puts (nil || false || 10 || true)
-    puts "---------------------------------------------------"
-    puts "When all values are falsy, the || operator returns the last given value (nil || false)"
-    puts (nil || false)
-    puts "---------------------------------------------------"
-    puts "&& operator returns the second given value only if the first one is truthy (:an_atom && 5)"
-    puts (:an_atom && 5)
-    puts "---------------------------------------------------"
-    puts "When the first given value is falsy, the first given value is returned instead (false && true)"
-    puts (false && true)
-    puts "---------------------------------------------------"
+  def tuples do
+    person = {"Robin", 20}
+    age = elem(person, 1)
+    name = elem(person, 0)
+
+    puts name
+    puts age
+
+    person = put_elem(person, 0, "Robin Nook")
+    name = elem(person, 0)
+
+    puts " "
+    puts name
+    puts age
   end
 end
