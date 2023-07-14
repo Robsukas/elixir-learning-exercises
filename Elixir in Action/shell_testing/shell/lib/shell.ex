511bfae9e7487868a18c623957ea2736237a4991
Robin Nook <robin.nook@yolo.com>
2023-07-14 13:53:06 +0300

    Added a new Exercism directory, where I do exercises.

--- Elixir in Action/shell_testing/shell/lib/shell.ex
+++ Elixir in Action/shell_testing/shell/lib/shell.ex
@@ -1,16 +1,3 @@
 defmodule Shell do
-  import IO
-  # Changed the system of code writing a bit,
-  # as pasting in big line breaks and comments got a bit too tedious.
-  # Also the code wasn't that well readable with those anyway.
-  # With this I also make the workflow more time-efficient.
-
-  def character_list do
-    puts 'ABC'
-    puts [65, 66, 67]
-    string = "String"
-    charlist = String.to_charlist(string)
-    puts string == charlist
-
-  end
+  # Placeholder push
 end
