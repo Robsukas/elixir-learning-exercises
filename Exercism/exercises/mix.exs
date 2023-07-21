4075d0615825bad506f25fd39aee2cb02d01c44e
Robin Nook <robin.nook@yolo.com>
2023-07-21 14:12:40 +0300

    EXERCISM: Form exercise added.

--- Exercism/exercises/mix.exs
+++ Exercism/exercises/mix.exs
@@ -21,6 +21,7 @@ defmodule Exercises.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:excoveralls, "~> 0.12.0", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
