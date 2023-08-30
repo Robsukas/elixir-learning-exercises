1b8e355be80aeae6b4f0688ff78bb75a161bcdfd
Robin Nook <robin.nook@yolo.com>
2023-08-30 10:42:46 +0300

    Created new separate directory for making an auction site with the help of Phoenix In Action book.

---
+++ Phoenix_In_Action/auction_umbrella/lib/auction_umbrella.ex
@@ -0,0 +1,18 @@
+defmodule AuctionUmbrella do
+  @moduledoc """
+  Documentation for `AuctionUmbrella`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AuctionUmbrella.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ Phoenix_In_Action/auction_umbrella/test/auction_umbrella_test.exs
@@ -0,0 +1,8 @@
+defmodule AuctionUmbrellaTest do
+  use ExUnit.Case
+  doctest AuctionUmbrella
+
+  test "greets the world" do
+    assert AuctionUmbrella.hello() == :world
+  end
+end
