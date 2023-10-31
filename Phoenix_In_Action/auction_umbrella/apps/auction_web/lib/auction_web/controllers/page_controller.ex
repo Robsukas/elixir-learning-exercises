0aa7f211c1d52e88dcdbe2221b84967ab99d2c2e
Robin Nook <robin.nook@yolo.com>
2023-10-31 14:57:25 +0200

    changes to packages and html

--- Phoenix_In_Action/auction_umbrella/apps/auction_web/lib/auction_web/controllers/page_controller.ex
+++ Phoenix_In_Action/auction_umbrella/apps/auction_web/lib/auction_web/controllers/page_controller.ex
@@ -4,6 +4,7 @@ defmodule AuctionWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    items = Auction.list_items()
+    render(conn, :home, layout: false, items: items)
   end
 end
