fcbe45f311b77486dd818cb0f5d1feba3648887c
Robin Nook <robin.nook@yolo.com>
2024-01-19 14:09:59 +0200

    ecto migrations working with new phoenix version etc.

--- Phoenix_In_Action/auction_umbrella/apps/auction/lib/auction/item.ex
+++ Phoenix_In_Action/auction_umbrella/apps/auction/lib/auction/item.ex
@@ -1,3 +1,8 @@
 defmodule Auction.Item do
-  defstruct [:id, :title, :description, :ends_at]
-end
+  use Ecto.Schema
+  schema "items" do
+    field :title, :string
+    field :description, :string
+    field :ends_at, :utc_datetime
+    timestamps()
+end end
