d0c7acc2024a3506933a7863b2b4c2f8ae501cb8
Robin Nook <robin.nook@yolo.com>
2024-02-05 15:46:59 +0200

    users table added, with ecto migration and changeset

--- Phoenix_In_Action/auction_umbrella/apps/auction/mix.exs
+++ Phoenix_In_Action/auction_umbrella/apps/auction/mix.exs
@@ -27,7 +27,9 @@ defmodule Auction.MixProject do
   defp deps do
     [
       {:ecto_sql, "~> 3.11"},
-      {:postgrex, "~> 0.17.4"}
+      {:postgrex, "~> 0.17.4"},
+      {:comeonin, "~> 5.4"},
+      {:pbkdf2_elixir, "~> 2.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
