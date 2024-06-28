4ade7b6473fdc76a8ad0013d72a85e150532a707
Robin Nook <robin.nook4@gmail.com>
2024-06-28 15:03:15 +0300

    survey completed, with product rating and demographic section

---
+++ Programming_Phoenix_Liveview/pento/lib/pento_web/live/demographic_live/form.ex
@@ -0,0 +1,57 @@
+defmodule PentoWeb.DemographicLive.Form do
+  use PentoWeb, :live_component
+
+  alias Pento.Survey
+  alias Pento.Survey.Demographic
+
+  def handle_event("validate", %{"demographic" => demographic_params}, socket) do
+    changeset =
+      %Demographic{}
+      |> Survey.change_demographic(demographic_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign_form(socket, changeset)}
+  end
+
+  def handle_event("save", %{"demographic" => demographic_params}, socket) do
+    params = params_with_user_id(demographic_params, socket)
+    {:noreply, save_demographic(socket, params)}
+  end
+
+  def update(assigns, socket) do
+    {
+      :ok,
+      socket
+      |> assign(assigns)
+      |> assign_demographic()
+      |> clear_form()}
+  end
+
+  def params_with_user_id(params, %{assigns: %{current_user: current_user}}) do
+    params
+    |> Map.put("user_id", current_user.id)
+  end
+
+  defp save_demographic(socket, demographic_params) do
+    case Survey.create_demographic(demographic_params) do
+      {:ok, demographic} ->
+        send(self(), {:created_demographic, demographic})
+        socket
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        assign_form(socket, changeset)
+    end
+  end
+
+  defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
+    assign(socket, :demographic, %Demographic{user_id: current_user.id})
+  end
+
+  defp assign_form(socket, changeset) do
+    assign(socket, :form, to_form(changeset))
+  end
+
+  defp clear_form(%{assigns: %{demographic: demographic}} = socket) do
+    assign_form(socket, Survey.change_demographic(demographic))
+  end
+end
--- Programming_Phoenix_Liveview/pento/lib/pento_web/live/survey_live.ex
+++ Programming_Phoenix_Liveview/pento/lib/pento_web/live/survey_live.ex
@@ -4,11 +4,58 @@ defmodule PentoWeb.SurveyLive do
   alias __MODULE__.Component
   alias Pento.Survey
   alias PentoWeb.DemographicLive
+  alias Pento.Catalog
+  alias PentoWeb.RatingLive.Show
 
   def mount(_params, _session, socket) do
     {:ok,
     socket
-    |> assign_demographic()}
+    |> assign_demographic()
+    |> assign_products()}
+  end
+
+  def handle_info({:created_demographic, demographic}, socket) do
+    {:noreply, handle_demographic_created(socket, demographic)}
+  end
+
+  def handle_info({:created_rating, updated_product, product_index}, socket) do
+    {:noreply, handle_rating_created(socket, updated_product, product_index)}
+  end
+
+  def handle_demographic_created(socket, demographic) do
+    socket
+    |> put_flash(:info, "Demographic created successfully")
+    |> assign(:demographic, demographic)
+  end
+
+  def handle_rating_created(
+    %{assigns: %{products: products}} = socket,
+    updated_product,
+    product_index) do
+      socket
+      |> put_flash(:info, "Rating submitted successfully") |> assign(
+        :products,
+        List.replace_at(products, product_index, updated_product)
+      )
+  end
+
+  def assign_products(%{assigns: %{current_user: current_user}} = socket) do
+    assign(socket, :products, list_products(current_user))
+  end
+
+  def product_rating(assigns) do
+    ~H"""
+      <div><%= @product.name %></div>
+      <%= if rating = List.first(@product.ratings) do %>
+        <Show.stars rating={rating} />
+      <% else %>
+        <h3><%= @product.name %> rating form coming soon!</h3>
+      <% end %>
+    """
+  end
+
+  defp list_products(user) do
+    Catalog.list_products_with_user_rating(user)
   end
 
   defp assign_demographic(%{assigns: %{current_user: current_user}}=socket) do
